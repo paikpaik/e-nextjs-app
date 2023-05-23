9aaae247a7d13258b080e888c4e4c491ed8772a5
Jaeyoul Ro <127716353+paikpaik@users.noreply.github.com>
2023-05-23 14:40:56 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Layout from "@/components/Layout";
+import Head from "next/head";
+
+export default function Home() {
+  console.log("Home")
+  return (
+    <>
+      <Head>
+        <title>WEB!</title>
+      </Head>
+        <h2>Welcome</h2>
+        Hello, World!
+    </>
+  );
+}
